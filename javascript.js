6c09a965eb5681f219c1e1c7a2744921197c3554
Kiryl21 <kirillevst1@gmail.com>
2025-02-12 16:08:22 +0100

    Create basic math operations functions^Cnd the operator function

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let sum = (a, b) => a + b;
+
+let substract = (a, b) => a - b;
+
+let multiply = (a, b) => a * b;
+
+let divide = (a, b) => a / b;
+
+let num1 = 0;
+
+let oper = undefined;
+
+let num2 = 0;
+
+let operator = (num1, oper, num2) => oper(num1, num2); 
